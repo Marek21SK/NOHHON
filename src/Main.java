32fb5ca1dd19fff070c3809122b08ce04680bd19
Marek Pástor <marekpastorml@gmail.com>
2022-02-09 12:23:06 +0100

    Java projekt knižnica

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,11 @@
 public class Main {
+    public static void main(String[] args) {
+        Kniha k1 = new Kniha();
+        k1.nazov = "Snehulienka a 7 trpaslíkov";
+        k1.autor = "Neviem uš";
+
+        Kniha k2 = new Kniha();
+        k2.nazov = "Transformers";
+        k2.autor = "Michael Bay";
+    }
 }
